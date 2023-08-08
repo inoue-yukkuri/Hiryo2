38ab1e123bf9a7ab1fd1030a5bf256bc7fae0bc6
Yuki Inoue <yukiinoue@Yukis-MacBook-Air.local>
2023-08-08 12:48:11 +0900

    Udemy見ながらの最低限のスクリーン

--- App.jsx
+++ App.jsx
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // 関数などの機能はここに書いてく
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <View style={styles.appbarInnner}>
+          <Text style={styles.appbarTitle}>肥料の最適くん</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/08/08</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/08/08</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/08/08</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +54,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F4F8',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appbarInnner: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 8,
+    color: 'rgba(255,255,255,0.8)',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 24,
+    lineHeight: 32,
+    color: '#ffffff',
+    fontWeight: 'bold',
   },
 });

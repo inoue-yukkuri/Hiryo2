7a2f7040199f6628e4bd758ab3e2ee96b4aac8c2
Yuki Inoue <yukiinoue@Yukis-MacBook-Air.local>
2023-11-03 13:33:07 +0900

    確認出せるようになった

--- App.jsx
+++ App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import {
+  BannerAd,
+  BannerAdSize, TestIds, AdsConsent, AdsConsentStatus, AdsConsentDebugGeography,
+// eslint-disable-next-line import/no-duplicates
+} from 'react-native-google-mobile-ads';
+// eslint-disable-next-line import/no-duplicates, no-unused-vars
+import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
 
 import AppBar from './src/components/AppBar';
 import InputScreen from './src/screens/InputScreen';
@@ -10,7 +16,38 @@ import OutputScreen from './src/screens/OutputScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [nonPersonalizedOnly, setNonPersonalizedOnly] = useState(true);
+
+  useEffect(() => {
+    const initializeAds = async () => {
+      // Initialize the Google Mobile Ads SDK
+      const adapterStatuses = await mobileAds().initialize();
+      console.log(adapterStatuses);
+
+      // Check for consent for EU users
+      const consentInfo = await AdsConsent.requestInfoUpdate({
+        debugGeography: AdsConsentDebugGeography.EEA, // EU圏としてテストする設定
+        testDeviceIdentifiers: ["TEST-DEVICE-HASHED-ID"],
+      });
+
+      let { status } = consentInfo;
+      if (consentInfo.isConsentFormAvailable && status === AdsConsentStatus.REQUIRED) {
+        // 同意状態が必要な場合はダイアログを表示する
+        const result = await AdsConsent.showForm();
+        status = result.status;
+      }
+
+      if (status === AdsConsentStatus.OBTAINED) {
+        // 同意が取得できた場合はNonPersonalizedOnlyをfalseにする(トラッキング取得する)
+        setNonPersonalizedOnly(false);
+      }
+    };
+
+    initializeAds();
+  }, []);
+
   const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+
   return (
     <NavigationContainer>
       <AppBar />
@@ -27,10 +64,9 @@ export default function App() {
         unitId={adUnitId}
         size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
         requestOptions={{
-          requestNonPersonalizedAdsOnly: true,
+          requestNonPersonalizedAdsOnly: nonPersonalizedOnly,
         }}
       />
     </NavigationContainer>
-
   );
 }
